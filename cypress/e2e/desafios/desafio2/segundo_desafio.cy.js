18fe926f8fe073f61575dd0d20eb0eab11eed770
Sebastian Salvador <sjsalvador.it@gmail.com.com>
2024-02-22 19:15:29 -0300

    desafio 1 corregido

---
+++ cypress/e2e/desafios/desafio2/segundo_desafio.cy.js
@@ -0,0 +1,41 @@
+import datosProducto from "./datosProducto";
+import { TIMEOUT } from "../../../fixtures/constantes"
+
+describe('Cypress Avanzado - Desafio 2', () => {
+  beforeEach(() => {
+    cy.login();
+  })
+
+  it('agregar buscar y eliminar un producto', () => {
+    //Ingresando a OnlineShop 
+    cy.get('a[data-cy="onlineshoplink"]', { timeout: TIMEOUT }).click()
+
+    //Agregando producto nuevo
+    cy.contains('Add Product').click()    
+    cy.get('input[data-cy="productName"]').type(datosProducto.nombre)
+    cy.get('input[data-cy="productPrice"]').type(datosProducto.precio)
+    cy.get('input[data-cy="productCard"]').type(datosProducto.url)
+    cy.get('input[data-cy="productID"]').type(datosProducto.id)
+    cy.contains('Create product').click()
+    cy.contains('button', 'Close' , { timeout: TIMEOUT } ).click();
+
+    //Busco producto por su ID
+    cy.get('[data-cy="search-type"]' , { timeout: TIMEOUT }).select('ID')
+    cy.get('input[data-cy="search-bar"]').type(`${datosProducto.id}{enter}`)
+    cy.get(`[data-cy="delete-${datosProducto.id}"]` , { timeout: TIMEOUT }).should('exist');
+
+
+    //Eliminando el producto
+    cy.get(`[data-cy="delete-${datosProducto.id}"]`, { timeout: TIMEOUT }).click()
+    cy.contains('button', 'Delete' , { timeout: TIMEOUT }).click();
+    cy.contains('button', 'Close' , { timeout: TIMEOUT }).click();
+
+    //Busco nuevamente el producto por su ID
+    cy.get('[data-cy="search-type"]' , { timeout: TIMEOUT }).select('ID')
+    cy.get('input[data-cy="search-bar"]').clear().type(`${datosProducto.id}{enter}`)
+    cy.get(`[data-cy="delete-${datosProducto.id}"]`, { timeout: TIMEOUT }).should('not.exist');
+
+
+  })
+})
+  
