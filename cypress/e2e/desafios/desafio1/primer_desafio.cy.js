60ad2325aa5d5e13c8c19a15361f6dc34d1b1e98
Sebastian Salvador <sjsalvador.it@gmail.com.com>
2024-02-22 19:13:45 -0300

    modificaciones ingresadas

--- cypress/e2e/desafios/desafio1/primer_desafio.cy.js
+++ cypress/e2e/desafios/desafio1/primer_desafio.cy.js
@@ -1,7 +1,7 @@
 import datosProducto from "./datosProducto";
+import { TIMEOUT } from "../../../fixtures/constantes"
 
 describe('Cypress Avanzado - Desafio 1', () => {
-  const TIMEOUT = 60000;
   beforeEach(() => {
     cy.login();
   })
@@ -17,23 +17,23 @@ describe('Cypress Avanzado - Desafio 1', () => {
     cy.get('input[data-cy="productCard"]').type(datosProducto.url)
     cy.get('input[data-cy="productID"]').type(datosProducto.id)
     cy.contains('Create product').click()
-    cy.contains('button', 'Close' , { timeout: TIMEOUT }).click();
+    cy.contains('button', 'Close' , { timeout: TIMEOUT } ).click();
 
     //Busco producto por su ID
     cy.get('[data-cy="search-type"]' , { timeout: TIMEOUT }).select('ID')
     cy.get('input[data-cy="search-bar"]').type(`${datosProducto.id}{enter}`)
-    cy.get('[data-cy="delete-3345"]' , { timeout: TIMEOUT }).should('exist');
+    cy.get(`[data-cy="delete-${datosProducto.id}"]` , { timeout: TIMEOUT }).should('exist');
 
 
     //Eliminando el producto
-    cy.get('[data-cy="delete-3345"]' , { timeout: TIMEOUT }).click()
+    cy.get(`[data-cy="delete-${datosProducto.id}"]`, { timeout: TIMEOUT }).click()
     cy.contains('button', 'Delete' , { timeout: TIMEOUT }).click();
     cy.contains('button', 'Close' , { timeout: TIMEOUT }).click();
 
     //Busco nuevamente el producto por su ID
     cy.get('[data-cy="search-type"]' , { timeout: TIMEOUT }).select('ID')
     cy.get('input[data-cy="search-bar"]').clear().type(`${datosProducto.id}{enter}`)
-    cy.get('[data-cy="delete-3345"]' , { timeout: TIMEOUT }).should('not.exist');
+    cy.get(`[data-cy="delete-${datosProducto.id}"]`, { timeout: TIMEOUT }).should('not.exist');
 
 
   })
---
+++ cypress/fixtures/constantes.js
@@ -0,0 +1,3 @@
+// constants.js
+const TIMEOUT = 10000; // Valor en milisegundos
+module.exports = { TIMEOUT };
